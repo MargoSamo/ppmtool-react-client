53424abfc78e53993f31842ba088de86b8728933
MargoSamo <margo_ii@mail.ru>
2022-09-19 16:09:23 +0400

    database connection, task submission

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject"; 
+import { Provider } from "react-redux"; 
+import store from "./store";
 
 function App() {
-  return (
-    <Router> 
-    <div className="App">
-      <Header />
-      <Dashboard /> 
-      {
-      //  <Route path="/addProject" component={AddProject} /> 
-      }
-    </div>
-    </Router>
-  );
-}
+  
+    return (
+      <Provider store={store}> 
+        <div className="App">
+          <Router> 
+            <Header />
+              <Routes> 
+                <Route exact path="/dashboard" element={<Dashboard/>} />
+                <Route exact path="/addProject" element={<AddProject/>} />      
+              </Routes>
+          </Router>
+        </div>
+      </Provider>
+    );
+  }
 
 export default App;
