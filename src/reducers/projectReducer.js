a7c1da791dbff2c4a9de346c628ac1b681537308
MargoSamo <margo_ii@mail.ru>
2022-09-25 19:24:00 +0400

    getting the project in the database

---
+++ src/reducers/projectReducer.js
@@ -0,0 +1,18 @@
+import { GET_PROJECTS } from "../actions/types";
+
+const initialState = {
+    pprojects: [],
+    project: {}
+};
+
+export default function(state = initialState, action){
+    switch (action.type) {
+        case GET_PROJECTS:
+            return {
+                ...state,
+                projects:action.payload
+            };
+        default:
+            return state;
+    }
+}
