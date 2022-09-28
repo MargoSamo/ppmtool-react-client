29936b603f9418f7f5bccaf00f8416fa6249f957
MargoSamo <margo_ii@mail.ru>
2022-09-28 19:27:44 +0400

    delete project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject"; 
 import { Provider } from "react-redux"; 
 import store from "./store";
@@ -11,17 +11,15 @@ import UpdateProject from './components/Project/UpdateProject';
 function App() {
   
     return (
-      <Provider store={store}> 
-        <div className="App">
-          <Router> 
+      <Provider store={store}>
+        <Router>
+          <div className="App">
             <Header />
-              <Routes> 
-                <Route exact path="/dashboard" element={<Dashboard/>} />
-                <Route exact path="/addProject" element={<AddProject/>} />      
-                <Route exact path="/updateProject/:id" element={<UpdateProject/>} />  
-              </Routes>
-          </Router>
-        </div>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
+          </div>
+        </Router>
       </Provider>
     );
   }
--- src/components/Project/UpdateProject.js
+++ src/components/Project/UpdateProject.js
@@ -3,7 +3,6 @@ import { getProject, createProject } from "../../actions/projectActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
-import { useParams } from 'react-router-dom';
 
 class UpdateProject extends Component {
 
@@ -16,7 +15,8 @@ class UpdateProject extends Component {
             projectIdentifier: "",
             description: "",
             start_date: "",
-            end_date: ""
+            end_date: "",
+            errors: {}
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this)
@@ -24,6 +24,11 @@ class UpdateProject extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
+
+        if(nextProps.errors){
+            this.setState({ errors:nextProps.errors });
+
+        }
         const {
             id,
             projectName,
@@ -44,9 +49,7 @@ class UpdateProject extends Component {
     }
 
     componentDidMount() {
-        // const { id } = this.props.match.params;
-        // this.props.getProject(id, this.props.history);
-
+    
         const { id } = this.props.match.params;
         this.props.getProject(id, this.props.history);
 
@@ -72,6 +75,7 @@ class UpdateProject extends Component {
       }
 
   render() {
+    const {errors} = this.state;
     return (
         <div className="project">
             <div className="container">
@@ -83,12 +87,17 @@ class UpdateProject extends Component {
                             <div className="form-group">
                                 <input 
                                 type="text" 
-                                className="form-control form-control-lg " 
+                                className={classnames("form-control form-control-lg", {
+                                    "is-invalid":errors.projectName
+                                })}
                                 placeholder="Project Name"
                                 name="projectName" 
                                 value={this.state.projectName}
                                 onChange={this.onChange}
                                 />
+                                {errors.projectName && (
+                                    <div className="invalid-feedback">{errors.projectName}</div>
+                                  )} 
                             </div>
                             <div className="form-group">
                                 <input type="text" 
@@ -101,12 +110,17 @@ class UpdateProject extends Component {
                             
                             <div className="form-group">
                                 <textarea 
-                                className="form-control form-control-lg" 
+                                className={classnames("form-control form-control-lg", {
+                                    "is-invalid":errors.description
+                                })} 
                                 placeholder="Project Description"
                                 name="description"
                                 value={this.state.description}
                                 onChange={this.onChange}
                                 ></textarea>
+                                {errors.description && (
+                                    <div className="invalid-feedback">{errors.description}</div>
+                                  )} 
                             </div>
                             <h6>Start Date</h6>
                             <div className="form-group">
@@ -142,11 +156,13 @@ class UpdateProject extends Component {
 UpdateProject.propTypes = {
     getProject: PropTypes.func.isRequired,
     createProject: PropTypes.func.isRequired,
-    project: PropTypes.object.isRequired
+    project: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
 }; 
 
 const mapStateToProps = state => ({
-    project: state.project.project
+    project: state.project.project,
+    errors: state.errors
 });
   
 
