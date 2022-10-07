bcb7143ce1d7b6f9d8a04691332e1062b7eaaebf
MargoSamo <margo_ii@mail.ru>
2022-10-07 20:58:42 +0400

    delete project task

--- src/actions/backlogActions.js
+++ src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types";
+import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from "./types";
 
 
 
@@ -71,6 +71,20 @@ export const getProjectTask = (
         dispatch({
             type: GET_ERRORS,
             payload: err.response.data
+        });
+    }
+  };
+
+  export const deleteProjectTask = (backlog_id, pt_id) => async dispatch => {
+    if(
+        window.confirm(
+            `You are deleting project task ${pt_id}, this action cannot be undone`
+        )
+    ) {
+        await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`);
+        dispatch({
+            type: DELETE_PROJECT_TASK,
+            payload: pt_id
         });
     }
   };
--- src/reducers/backlogReducer.js
+++ src/reducers/backlogReducer.js
@@ -16,22 +16,21 @@ const initialState = {
             return{
                 ...state,
                 project_tasks: action.payload
-            }
+            };
 
         case GET_PROJECT_TASK:
             return{
             ...state,
             project_task: action.payload
-
-            }
+            };
 
         case DELETE_PROJECT_TASK:
             return{
-            ...state
-            
-            //TO_DO
-
-            }
+            ...state, 
+            project_tasks: state.project_tasks.filter(
+                project_task => project_task.projectSequence !== action.payload
+                )          
+            };
 
         default:
             return state;
